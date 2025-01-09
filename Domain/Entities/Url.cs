10f842d2608ed0141d03e87a53b4af13f835deab
mateu <mateuscizeski@gmail.com>
2025-01-08 21:37:48 -0300

    Entidades.

--- Domain/Entities/Url.cs
+++ Domain/Entities/Url.cs
@@ -1,9 +1,30 @@
-﻿namespace Domain.Entities
+﻿using System.Text.RegularExpressions;
+
+namespace Domain.Entities
 {
     public class Url : Entity
     {
         public string  ShortinedUrl { get; private set; }
         public string OriginalUrl { get; private set; }
         public DateTime ExpirationDate { get; private set; }
+
+        public Url(string shortenedUrl, string originalUrl, DateTime expirationDate)
+        {
+            if(!IsValidUrl(originalUrl))
+            {
+                throw new ArgumentException("The provided URL is not valid.");
+            }
+
+            ShortinedUrl = shortenedUrl;
+            OriginalUrl = originalUrl;
+            ExpirationDate = expirationDate;
+        }
+
+        private bool IsValidUrl(string url)
+        {
+            var regex = new Regex(@"^https?:\/\/[\w\-\.]+(\.[\w\-]+)+[/#?]?.*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+       
+            return regex.IsMatch(url);
+        }
     }
 }
