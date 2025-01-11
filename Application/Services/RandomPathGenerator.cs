1fb1f22aa6b2b32cdf13530cddeab1f4ad5e7f5a
mateu <mateuscizeski@gmail.com>
2025-01-10 23:40:12 -0300

    Métodos do services.

--- Application/Services/RandomPathGenerator.cs
+++ Application/Services/RandomPathGenerator.cs
@@ -1,6 +1,29 @@
-﻿namespace Application.Services
+﻿using Application.Interfaces;
+using System.Text;
+
+namespace Application.Services
 {
-    internal class RandomPathGenerator
+    internal class RandomPathGenerator : IRandomPathGenerator
     {
+        private readonly Random _random;
+        private const string _chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        public RandomPathGenerator()
+        {
+            _random = new();
+        }
+
+        public string GenerateRandomPath(int minLength, int maxLength)
+        {
+            int length = _random.Next(minLength, maxLength + 1);
+            StringBuilder result = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(_chars[_random.Next(_chars.Length)]);
+            }
+
+            return result.ToString();
+        }
     }
 }
--- Application/Services/UrlService.cs
+++ Application/Services/UrlService.cs
@@ -1,6 +1,85 @@
-﻿namespace Application.Services
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Domain.Entities;
+using Domain.Errors;
+using Domain.Interfaces;
+using Infrastructure.Common.Interfaces;
+
+namespace Application.Services
 {
-    internal class UrlService
+    public class UrlService : IUrlService
     {
+        private const int MinLength = 5;
+        private const int MaxLength = 10;
+
+        private readonly IUrlRepository _urlRepository;
+        private readonly ISystemTime _clock;
+        private readonly IRandomPathGenerator _randomPathGenerator;
+
+        public UrlService(IUrlRepository urlRepository, ISystemTime clock, IRandomPathGenerator randomPathGenerator)
+        {
+            _urlRepository = urlRepository;
+            _clock = clock;
+            _randomPathGenerator = randomPathGenerator;
+        }
+
+        public async Task<Url> CreateShortUrl(CreateShortUrlDto dto)
+        {
+            string? shortenedUrl = dto.ShortenedUrl;
+            if (!string.IsNullOrEmpty(shortenedUrl))
+            {
+                if (shortenedUrl.Length > MaxLength || shortenedUrl.Length < MinLength)
+                {
+                    throw new NotValidDataException("The provided path must be at least 5 and not more then 10 chars long.");
+                }
+
+                var pathAlreadyInUse = await _urlRepository.IsPathInUse(shortenedUrl);
+
+                if (pathAlreadyInUse)
+                {
+                    throw new NotValidDataException("The provided path is not avalible");
+                }
+            }
+            else
+            {
+                shortenedUrl = _randomPathGenerator.GenerateRandomPath(MinLength, MaxLength);
+            }
+
+
+            Url newUrl = new(shortenedUrl, dto.OriginalUrl, _clock.UtcNow.AddDays(7));
+            newUrl.CreatedAt = _clock.UtcNow;
+
+            Url createdUrl = await _urlRepository.AddShortUrl(newUrl);
+            return createdUrl;
+        }
+
+        public async Task DeleteUrl(Guid id)
+        {
+            await _urlRepository.DeleteUrlById(id);
+        }
+
+        public async Task<IEnumerable<Url>> GetAllUrls()
+        {
+            return await _urlRepository.GetAllShortUrls();
+        }
+
+        public async Task<Url?> GetUrlById(Guid id)
+        {
+            Url? url = await _urlRepository.GetUrlById(id);
+
+            if (url is null)
+            {
+                throw new ResourceNotFoundException($"Url Entity with the provided ID {id} was not found.");
+            }
+
+            return url;
+        }
+
+        public async Task<string> GetUrlByShortenedUrl(string url)
+        {
+            Url urlEntity = await _urlRepository.GetUrlByShortenedUrl(url);
+
+            return urlEntity.OriginalUrl;
+        }
     }
 }
